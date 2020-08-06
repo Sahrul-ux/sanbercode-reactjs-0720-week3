e35b37189ce5a96c1819b46a0f72d8d5249a10ba
Sahrul-ux <sahrulrajab11@gmail.com>
2020-08-06 22:26:59 +0700

    belajar crud reactjs

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './App.css';
 import 	DaftarBuah from './tugas11/DaftarBuah';
 import  Timer from './tugas12/Timer';
+import Lists from './tugas13/Lists';
 
 
 function App() {
-return (
-    <div>
-      <DaftarBuah />
-      <Timer start={10} />
-    </div>
-  );
+	return (
+	    <div>
+	     {/*<DaftarBuah />*/}
+	     {/*<Timer start={10} />*/}
+	    	<Lists/>
+	    </div>
+	);
 }
 
 
