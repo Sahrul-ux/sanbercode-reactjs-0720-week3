f8db5600f3cba4d9583540c7c35e92b61fb93d62
Sahrul-ux <sahrulrajab11@gmail.com>
2020-08-07 22:00:11 +0700

    belajar API dan Axios

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import 	DaftarBuah from './tugas11/DaftarBuah';
 import  Timer from './tugas12/Timer';
 import Lists from './tugas13/Lists';
+import Table3 from './tugas14/Table3'
 
 
 function App() {
 	return (
-	    <div>
+	    <div className="app">
 	     {/*<DaftarBuah />*/}
 	     {/*<Timer start={10} />*/}
-	    	<Lists/>
+	    	{/*<Lists/>*/}
+	    <Timer start={101}/>
+	    <Table3/>
 	    </div>
 	);
 }
